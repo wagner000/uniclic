b754ee30a56158e4c7c9fed0d2befbb6ab8295c0
Wagner R Pinto <wagner.ufma@gmail.com>
2019-03-12 15:44:24 -0300

    mascara no cad paciente

--- sistemaclinica/src/main/java/com/sistemaclinica/model/Paciente.java
+++ sistemaclinica/src/main/java/com/sistemaclinica/model/Paciente.java
@@ -17,7 +17,6 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.br.CPF;
 
 @Entity
@@ -64,8 +63,8 @@ public class Paciente implements Serializable {
 		this.nome = nome;
 	}
 	
-	@NotEmpty @Email
-	@Column(unique = false)
+	@Email
+	@Column(unique = true)
 	public String getEmail() {
 		return email;
 	}
